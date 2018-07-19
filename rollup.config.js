9ef650d16a464873bbf0ee23192742998c16adf0
Lionel T <elrumordelaluz@hotmail.com>
2018-07-19 23:16:52 +0200

    Update rollup. Fix options renamed

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,20 @@ import pkg from './package.json'
 export default [
   // browser-friendly UMD build
   {
-    entry: 'src/index.js',
-    dest: pkg.browser,
-    format: 'umd',
-    moduleName: 'elementToPath',
+    input: 'src/index.js',
+    output: {
+      file: pkg.browser,
+      name: 'elementToPath',
+      format: 'umd',
+    },
   },
 
   // CommonJS (for Node) and ES module (for bundlers) build.
   {
-    entry: 'src/index.js',
-    targets: [
-      { dest: pkg.main, format: 'cjs' },
-      { dest: pkg.module, format: 'es' },
+    input: 'src/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
     ],
   },
 ]
